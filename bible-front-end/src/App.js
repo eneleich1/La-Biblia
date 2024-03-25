7ed1bcdf4a0732f52068d3cfb650e2b285b92da0
Nestor Longa <eneleich1@gmail.com>
2024-03-25 03:23:31 -0400

    Render Genesis1 from json file

--- bible-front-end/src/App.js
+++ bible-front-end/src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component1 } from './components/Component1';
+import { Genesis1_Book } from './components/Genesis1_Book';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Component1/>
-      </header>
+      <Genesis1_Book/>
     </div>
   );
 }
