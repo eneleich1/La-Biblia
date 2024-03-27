52496a1d90b060d887698435f966345d45fee43f
Nestor Longa <eneleich1@gmail.com>
2024-03-27 00:13:57 -0400

    Complete BibleHome component

--- bible-front-end/src/App.js
+++ bible-front-end/src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component1 } from './components/Component1';
-import { Genesis1_Book } from './components/Genesis1_Book';
-import { BibleHome } from './components/BibleHome';
+import BibleHome from './components/BibleHome';
+
 
 
 function App() {
