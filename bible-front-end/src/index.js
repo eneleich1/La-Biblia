05c29322cb0391712f7e54b231d81a681eab70ff
Nestor Longa <eneleich1@gmail.com>
2024-03-26 20:34:24 -0400

    Add Bible Content assets
    Add .css files
    Add images
    Add fonts

--- bible-front-end/src/index.js
+++ bible-front-end/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import './Styles/Content/css/indexPageStyle.css'
+import './Styles/Content/css/indexStyle.css'
+import './Styles/Content/css/mystyle.css'
+import './Styles/Content/css/standarPageStyle.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
