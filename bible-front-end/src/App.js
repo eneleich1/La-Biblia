d9343a7893f407f99c7abfab7681ef384d4a0be1
Nestor Longa <eneleich1@gmail.com>
2024-03-26 20:17:39 -0400

    Create BibleHome Component

--- bible-front-end/src/App.js
+++ bible-front-end/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Component1 } from './components/Component1';
 import { Genesis1_Book } from './components/Genesis1_Book';
+import { BibleHome } from './components/BibleHome';
 
 
 function App() {
   return (
     <div className="App">
-      <Genesis1_Book/>
+      <BibleHome/>
     </div>
   );
 }
---
+++ bible-front-end/src/components/BibleHome.js
@@ -0,0 +1,466 @@
+import React from 'react'
+
+export const BibleHome = () => {
+  return (
+    <div class="d-flex flex-column">
+
+		<div class="d-flex">
+			<div hidden class="bg-gray flex-fill">
+				<div class="container pb-30 pt-30 bg-blue">
+					<div class="container">
+						<h2 class="pb-30">Evangelio de Hoy</h2>
+						<div>
+							<ul>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>Cena del Señor</p>
+								</div>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>Yo soy el pan vivo bajado del cielo</p>
+								</div>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>Quien come mi cuerpo y bebe mi sangre tandra vida eterna</p>
+								</div>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>Y el pan que dare es mi cuerpo</p>
+								</div>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>Quien comoe y bebe sin discernir el cuerpo, como y bebe su propia condenacion</p>
+								</div>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class=" d-flex flex-column flex-fill">
+
+				<div class="text-center pb-40 pt-30">
+					<h2>La Biblia de Jerusalén</h2>
+					<h3>Edición de 1976</h3>
+				</div>
+
+				<div class="d-sm-flex flex-wrap justify-content-center mt-5">
+
+					<div id="oldTestament_div" class="bg-white pb-30 ">
+						<div class="ml-2">
+							<h2 class="pb-50 text-center">Antiguo Testamento</h2>
+							<div class="mt-5">
+								<div>
+									<h4>Pentatéuco</h4>
+									<ul>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/01- GÉNESIS.html'>01- Génesis</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/02- ÉXODO.html'>02- Éxodo</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/03- LEVÍTICO.html'>03- Levítico</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/04- NÚMEROS.html'>04- Números</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/05- DEUTERONOMIO.html'>05-
+												Deuteronomio</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Libros Históricos</h4>
+									<ul>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/06- JOSUÉ.html'>06- Josué</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/07- JUECES.html'>07- Jueces</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/08- RUT.html'>08- Rut</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/09- LIBRO PRIMERO DE SAMUEL.html'>09-
+												1
+												Samuel</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/10- LIBRO SEGUNDO DE SAMUEL.html'>10-
+												2
+												Samuel</a>
+										</li>
+										<li>
+											<a
+												href='00 - Antiguo Testamento/00- Indice/11- LIBRO PRIMERO DE LOS REYES.html'>11-
+												1
+												Reyes</a>
+										</li>
+										<li>
+											<a
+												href='00 - Antiguo Testamento/00- Indice/12- LIBRO SEGUNDO DE LOS REYES.html'>12-
+												2
+												Reyes</a>
+										</li>
+										<li>
+											<a
+												href='00 - Antiguo Testamento/00- Indice/13- LIBRO PRIMERO DE LAS CRÓNICAS.html'>13-
+												1 Crónicas</a>
+										</li>
+										<li>
+											<a
+												href='00 - Antiguo Testamento/00- Indice/14- LIBRO SEGUNDO DE LAS CRÓNICAS.html'>14-
+												2 Crónicas</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/15- ESDRAS.html'>15- Esdras</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/16- NEHEMÍAS.html'>16- Nehemías</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/17- TOBÍAS.html'>17- Tobías</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/18- JUDIT.html'>18- Judit</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/19- ESTER.html'>19- Ester</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/20- I MACABEOS.html'>20- I
+												Macabeos</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/21- II MACABEOS.html'>21- II
+												Macabeos</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Libros Poéticos o de Sabiduría</h4>
+									<ul>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/22- JOB.html'>22- Job</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/23- LOS SALMOS.html'>23- Los
+												Salmos</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/24- PROVERBIOS.html'>24-
+												Proverbios</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/25- ECLESIASTÉS.html'>25-
+												Eclesiastés</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/26- CANTAR DE LOS CANTARES.html'>26-
+												Cantar</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/27- SABIDURÍA.html'>27-
+												Sabiduría</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/28- ECLESIÁSTICO.html'>28-
+												Sirácide (Eclesiástico)</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Profetas Mayores</h4>
+									<ul>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/29- ISAÍAS.html'>29- Isaías</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/30- JEREMÍAS.html'>30- Jeremías</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/31- LAMENTACIONES.html'>31-
+												Lamentaciones</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/33- EZEQUIEL.html'>33- Ezequiel</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/34- DANIEL.html'>34- Daniel</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Profetas Menores</h4>
+									<ul>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/32- BARUC.html'>32- Baruc</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/35- OSEAS.html'>35- Oseas</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/36- JOEL.html'>36- Joel</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/37- AMÓS.html'>37- Amós</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/38- ABDÍAS.html'>38- Abdías</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/39- JONÁS.html'>39- Jonás</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/40- MIQUEAS.html'>40- Miqueas</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/41- NAHÚM.html'>41- Nahúm</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/42- HABACUC.html'>42- Habacuc</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/43- SOFONÍAS.html'>43- Sofonías</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/44- AGEO.html'>44- Ageo</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/45- ZACARÍAS.html'>45- Zacarías</a>
+										</li>
+										<li>
+											<a href='00 - Antiguo Testamento/00- Indice/46- MALAQUÍAS.html'>46-
+												Malaquías</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div hidden class="verticalLine"></div>
+
+					<div class="bg-white  pb-30">
+						<div class="ml-2">
+							<h2 class="pb-50 text-center">Nuevo Testamento</h2>
+							<div class="mt-5">
+								<div>
+									<h4>Los Evangelios</h4>
+									<ul>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/01- EVANGELIO SEGÚN SAN MATEO.html'>01-
+												Mateo</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/02- EVANGELIO SEGÚN SAN MARCOS.html'>02-
+												Marcos</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/03- EVANGELIO SEGÚN SAN LUCAS.html'>03-
+												Lucas</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/04- EVANGELIO SEGÚN SAN JUAN.html'>04-
+												Juan</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Hechos</h4>
+									<ul>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/05- HECHOS DE LOS APÓSTOLES.html'>05-
+												Hechos</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Cartas de Pablo</h4>
+									<ul>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/06- EPÍSTOLA A LOS ROMANOS.html'>06-
+												Romanos</a>
+										</li>
+										<li>
+											<a
+												href='01 - Nuevo Testamento/00- Indice/07- PRIMERA EPÍSTOLA A LOS CORINTIOS.html'>07-
+												1 Corintios</a>
+										</li>
+										<li>
+											<a
+												href='01 - Nuevo Testamento/00- Indice/08- SEGUNDA EPÍSTOLA A LOS CORINTIOS.html'>08-
+												2 Corintios</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/09- EPÍSTOLA A LOS GÁLATAS.html'>09-
+												Gálatas</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/10- EPÍSTOLA A LOS EFESIOS.html'>10-
+												Efesios</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/11- EPÍSTOLA A LOS FILIPENSES.html'>11-
+												Filipenses</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/12- EPÍSTOLA A LOS COLOSENSES.html'>12-
+												Colosenses</a>
+										</li>
+										<li>
+											<a
+												href='01 - Nuevo Testamento/00- Indice/13- PRIMERA EPÍSTOLA A LOS TESALONICENSES.html'>13-
+												1 Tesalonicenses</a>
+										</li>
+										<li>
+											<a
+												href='01 - Nuevo Testamento/00- Indice/14- SEGUNDA EPÍSTOLA A LOS TESALONICENSES.html'>14-
+												2 Tesalonicenses</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/15- PRIMERA EPÍSTOLA A TIMOTEO.html'>15-
+												1
+												Timoteo</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/16- SEGUNDA EPÍSTOLA A TIMOTEO.html'>16-
+												2
+												Timoteo</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/17- EPÍSTOLA A TITO.html'>17- Tito</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/18- EPÍSTOLA A FILEMÓN.html'>18-
+												Filemón</a>
+										</li>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/19- EPÍSTOLA A LOS HEBREOS.html'>19-
+												Hebreos</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Santiago</h4>
+									<ul>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/20- EPÍSTOLA DE SANTIAGO.html'>20-
+												Santiago</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Pedro</h4>
+									<ul>
+										<li>
+											<a
+												href='01 - Nuevo Testamento/00- Indice/21- PRIMERA EPÍSTOLA DE SAN PEDRO.html'>21-
+												1
+												Pedro</a>
+										</li>
+										<li>
+											<a
+												href='01 - Nuevo Testamento/00- Indice/22- SEGUNDA EPÍSTOLA DE SAN PEDRO.html'>22-
+												2
+												Pedro</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Juan</h4>
+									<ul>
+										<li>
+											<a
+												href='01 - Nuevo Testamento/00- Indice/23- PRIMERA EPÍSTOLA DE SAN JUAN.html'>23-
+												1
+												Juan</a>
+										</li>
+										<li>
+											<a
+												href='01 - Nuevo Testamento/00- Indice/24- SEGUNDA EPÍSTOLA DE SAN JUAN.html'>24-
+												2
+												Juan</a>
+										</li>
+										<li>
+											<a
+												href='01 - Nuevo Testamento/00- Indice/25- TERCERA EPÍSTOLA DE SAN JUAN.html'>25-
+												3
+												Juan</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Judas</h4>
+									<ul>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/26- EPÍSTOLA DE SAN JUDAS.html'>26-
+												Judas</a>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4>Apocalipsis</h4>
+									<ul>
+										<li>
+											<a href='01 - Nuevo Testamento/00- Indice/27- APOCALIPSIS.html'>27-
+												Apocalipsis</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<footer>
+					Copyright © Eneleich Company
+				</footer>
+			</div>
+
+			<div hidden class="bg-sea flex-fill">
+				<div class="container pb-30 pt-30 bg-gray">
+					<div class="container" >
+						<h2>Estudio Diario</h2>
+						<ul>
+							<li>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>Cena del Señor</p>
+								</div>
+							</li>
+							<li>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>Una sola Iglesia</p>
+								</div>
+							</li>
+							<li>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>Bautismo de Infantes</p>
+								</div>
+							</li>
+							<li>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>Resurrecion de los muertos</p>
+								</div>
+							</li>
+							<li>
+								<div>
+									<a href="">Mateo 1: 24</a>
+									<p>El Juicio final</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  )
+}
