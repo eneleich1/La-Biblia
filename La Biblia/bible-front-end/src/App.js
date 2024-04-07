484cc9de74229fdd7b229886352e4eb0a55ccb0f
Nestor Longa <eneleich1@gmail.com>
2024-04-07 16:34:36 -0400

    Fix babel-preset-react-app
    https://stackoverflow.com/questions/76435306/babel-preset-react-app-is-importing-the-babel-plugin-proposal-private-propert

--- La Biblia/bible-front-end/src/App.js
+++ La Biblia/bible-front-end/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import BibleHome from './components/BibleHome';
-import Chapter from './components/Chapter';
 import BookIndex from './components/BookIndex';
 
 
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" Component={BibleHome}/>
         <Route path="/:testament/:bookIndex" Component={BookIndex}/>
-        <Route path="/:chapter/:index" Component={Chapter}/>
       </Routes>
     </Router>
   );
