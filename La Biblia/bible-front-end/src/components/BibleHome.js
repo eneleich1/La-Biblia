19ec2f97b22c1338fa024c70cb2d94b294d87a07
Nestor Longa <eneleich1@gmail.com>
2024-04-07 20:38:43 -0400

    Convert BibleHome in a Functional component

--- La Biblia/bible-front-end/src/components/BibleHome.js
+++ La Biblia/bible-front-end/src/components/BibleHome.js
@@ -1,68 +1,65 @@
-import React, { Component } from 'react'
 import newtest from '../data/nuevo-testamento.json'
 import oldtest from '../data/antiguo-testamento.json'
 import bookConfigs from '../data/bookConfigs.json'
 
-class BibleHome extends Component {
+export const bibleHome = () => {
 
-  GroupBookView(gb, testament) {
-    return (
+  const GroupBookView = (gb, testament) => {
+    return(
       <div className='mt-5 text-start' key={gb.Title}>
-        <h4>{gb.Title !== testament.Title ? gb.Title : ""}</h4>
-        <ul>
+      <h4>{gb.Title !== testament.Title ? gb.Title : ""}</h4>
+      <ul>
+        {
+          gb.BookIndexes.map(i =>
+            <li key={i}>
+              <a className='title-case a-link' href='00 - Antiguo Testamento/00- Indice/01- GÉNESIS.html'>
+                {
+                  testament.Books[i - 1].Title.toLocaleLowerCase()
+                }
+              </a>
+            </li>
+          )
+        }
+      </ul>
+    </div>
+    )
+  }
+
+  const TestamentView = (booksGroups, testament) => {
+    return(
+      <div id="oldTestament_div" className="pb-30 ">
+      <div className="ml-2">
+        <h2 className="pb-30 text-center">{testament.Title}</h2>
+        <div className="mt-5">
           {
-            gb.BookIndexes.map(i =>
-              <li key={i}>
-                <a className='title-case a-link' href='00 - Antiguo Testamento/00- Indice/01- GÉNESIS.html'>
-                  {
-                    testament.Books[i - 1].Title.toLocaleLowerCase()
-                  }
-                </a>
-              </li>
+            booksGroups.map(gb =>
+              <div className="mt-5" key={gb.Title}>
+                {GroupBookView(gb, testament)}
+              </div>
             )
           }
-        </ul>
-      </div>
-    )
-  }
-
-  TestamentView(booksGroups, testament) {
-    return (
-      <div id="oldTestament_div" className="pb-30 ">
-        <div className="ml-2">
-          <h2 className="pb-30 text-center">{testament.Title}</h2>
-          <div className="mt-5">
-            {
-              booksGroups.map(gb =>
-                  <div className="mt-5" key={gb.Title}>
-                    {this.GroupBookView(gb, testament)}
-                  </div>
-              )
-            }
-          </div>
         </div>
       </div>
+    </div>
     )
   }
 
-  GetBooksGroups(booksConfig){
+  const GetBooksGroups = (booksConfig)=>{
     const otg = [];
     const ntg = [];
     booksConfig.BooksGroups.forEach(bg => {
-      if(bg.Testament === 1) otg.push(bg)
-      else if(bg.Testament === 2) ntg.push(bg)
+      if (bg.Testament === 1) otg.push(bg)
+      else if (bg.Testament === 2) ntg.push(bg)
     });
 
-    return {oldTestGroups: otg, newTestGroups: ntg}
+    return { oldTestGroups: otg, newTestGroups: ntg }
   }
 
-  render() {
-    const defaultConfig = bookConfigs[1];
-    const booksGroups = this.GetBooksGroups(defaultConfig);
+  const defaultConfig = bookConfigs[1];
+  const booksGroups = GetBooksGroups(defaultConfig);
 
-    return (
-      <div className=" d-flex flex-column flex-fill">
-
+  return (
+    <div className=" d-flex flex-column flex-fill">
         <div className="text-center pb-40 pt-30">
           <h2>La Biblia de Jerusalén</h2>
           <h3>Edición de 1976</h3>
@@ -70,9 +67,9 @@ class BibleHome extends Component {
 
         <div className="d-sm-flex flex-wrap justify-content-center mt-5">
 
-          {this.TestamentView(booksGroups.oldTestGroups, oldtest)}
+          {TestamentView(booksGroups.oldTestGroups, oldtest)}
           <div className="verticalLine" />
-          {this.TestamentView(booksGroups.newTestGroups, newtest)}
+          {TestamentView(booksGroups.newTestGroups, newtest)}
 
         </div>
 
@@ -80,9 +77,7 @@ class BibleHome extends Component {
           Copyright © Eneleich Company
         </footer>
       </div>
-    )
-  }
-
+  )
 }
 
-export default BibleHome;
+export default bibleHome;
