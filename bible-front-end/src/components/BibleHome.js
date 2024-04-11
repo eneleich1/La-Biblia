4827b55895103066bda9562d746f2bd84fa2f9a5
Nestor Longa <eneleich1@gmail.com>
2024-04-10 22:41:46 -0400

    Use Separated Functions in BibleHome Component

--- bible-front-end/src/components/BibleHome.js
+++ bible-front-end/src/components/BibleHome.js
@@ -1,65 +1,62 @@
 import newtest from '../data/nuevo-testamento.json'
 import oldtest from '../data/antiguo-testamento.json'
 import bookConfigs from '../data/bookConfigs.json'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-export const bibleHome = () => {
+function GetBooksGroups(booksConfig) {
+  const otg = [];
+  const ntg = [];
+  booksConfig.BooksGroups.forEach(bg => {
+    if (bg.Testament === 1) otg.push(bg)
+    else if (bg.Testament === 2) ntg.push(bg)
+  });
 
-  const GroupBookView = (gb, testament) => {
+  return { oldTestGroups: otg, newTestGroups: ntg }
+}
 
-    //let test = gb.Testament ===1? "Antiguo-Testamento":"Nuevo-Testamento"
+function GroupBookView(gb, testament) {
+  return (
+    <div className='mt-5 text-start' key={gb.Title}>
+      <h2>{gb.Title !== testament.Title ? gb.Title : ""}</h2>
+      <ul>
+        {
+          gb.BookIndexes.map(i =>
+            <li key={i}>
+              <Link to={`/${gb.Testament}/${i}`}>
+                <p className='title-case a-link'>
+                  {
+                    testament.Books[i - 1].Title.toLocaleLowerCase()
+                  }
+                </p>
+              </Link>
+            </li>
+          )
+        }
+      </ul>
+    </div>
+  )
+}
 
-    return (
-      <div className='mt-5 text-start' key={gb.Title}>
-        <h4>{gb.Title !== testament.Title ? gb.Title : ""}</h4>
-        <ul>
+function TestamentView(booksGroups, testament) {
+  return (
+    <div id="oldTestament_div" className="pb-30 ">
+      <div className="ml-2">
+        <h1 className="pb-30 text-center">{testament.Title}</h1>
+        <div className="mt-5">
           {
-            gb.BookIndexes.map(i =>
-              <li key={i}>
-                <Link to={`/${gb.Testament}/${i}`}>
-                  <p className='title-case a-link'>
-                    {
-                      testament.Books[i - 1].Title.toLocaleLowerCase()
-                    }
-                  </p>
-                </Link>
-              </li>
+            booksGroups.map(gb =>
+              <div className="mt-5" key={gb.Title}>
+                {GroupBookView(gb, testament)}
+              </div>
             )
           }
-        </ul>
-      </div>
-    )
-  }
-
-  const TestamentView = (booksGroups, testament) => {
-    return (
-      <div id="oldTestament_div" className="pb-30 ">
-        <div className="ml-2">
-          <h2 className="pb-30 text-center">{testament.Title}</h2>
-          <div className="mt-5">
-            {
-              booksGroups.map(gb =>
-                <div className="mt-5" key={gb.Title}>
-                  {GroupBookView(gb, testament)}
-                </div>
-              )
-            }
-          </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
-  const GetBooksGroups = (booksConfig) => {
-    const otg = [];
-    const ntg = [];
-    booksConfig.BooksGroups.forEach(bg => {
-      if (bg.Testament === 1) otg.push(bg)
-      else if (bg.Testament === 2) ntg.push(bg)
-    });
-
-    return { oldTestGroups: otg, newTestGroups: ntg }
-  }
+function BibleHome() {
 
   const defaultConfig = bookConfigs[1];
   const booksGroups = GetBooksGroups(defaultConfig);
@@ -86,4 +83,4 @@ export const bibleHome = () => {
   )
 }
 
-export default bibleHome;
+export default BibleHome;
